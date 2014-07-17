7ff41822b8b2db310383307ff11374e4cdcb7403
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-07-17 16:05:54 +0200

    uh oh

--- src/main/scala/ohnosequences/scarph/Index.scala
+++ src/main/scala/ohnosequences/scarph/Index.scala
@@ -2,16 +2,31 @@ package ohnosequences.scarph
 
 trait AnyIndex extends Denotation[AnyIndexType] { index =>
 
-  type IndexedType //= tpe.IndexedType
-  type PredicateType //= tpe.PredicateType
-  type Out //<: tpe.Out[item.Rep]
+  type IndexedType = tpe.IndexedType
+  type PredicateType = tpe.PredicateType
+  // type Out <: Tpe#Out[item.Rep]
 
   // TODO: couldn't place this bound
   type Item <: AnyDenotation //.Of[IndexedType]
   val  item: Item
 
   abstract class LookupItem[P <: PredicateType](val predicate: P) {
-    def apply(rep: index.Rep): index.Out
-  }
 
+    type Out = index.tpe.Out[index.item.Rep]
+    def apply(rep: index.Rep): Out
+  }
+  
+
+}
+
+trait AnyStandardIndex extends AnyIndex { index =>
+
+  type Tpe <: AnyStandardIndexType
+  val tpe: Tpe
+
+  abstract class LookupItem[P <: PredicateType](val predicate: P) {
+
+    type Out = index.tpe.Out[index.item.Rep]
+    def apply(rep: index.Rep): Out
+  }
 }
--- src/main/scala/ohnosequences/scarph/titan/TitanIndex.scala
+++ src/main/scala/ohnosequences/scarph/titan/TitanIndex.scala
@@ -5,25 +5,28 @@ import com.thinkaurelius.titan.core._
 import com.thinkaurelius.titan.core.attribute.Cmp._
 import scala.collection.JavaConversions._
 
-trait AnyTitanStandardIndex extends AnyIndex { tindex =>
+trait AnyTitanStandardIndex extends AnyStandardIndex { tindex =>
 
   type Tpe <: AnyStandardIndexType
+  val tpe: Tpe
 
   type Raw = com.thinkaurelius.titan.core.TitanGraph
 
   // FIXME: couldn't avoid it: (should be just set once in AnyIndex type)
-  type IndexedType = tindex.tpe.IndexedType
-  type PredicateType = tindex.tpe.PredicateType
-  type Out = tindex.tpe.Out[tindex.item.Rep]
+  // type IndexedType = tpe.IndexedType
+  // type PredicateType = tindex.tpe.PredicateType
+  // type Out = tindex.tpe.Out[tindex.item.Rep]
+
+  // type Out = tpe.Out[item.Rep]
 
   implicit def lookupper[P <: tindex.PredicateType 
                          with AnyVertexPredicate
                          with AnyPredicate.HeadedBy[EQ[tpe.Property]] 
-                  ](p: P): LookupItem[P] =
+                  ](p: P): this.LookupItem[P] =
     new LookupItem[P](p) {
 
-      def apply(rep: tindex.Rep): tindex.Out = {
-        val it = rep.query().has(p.head.property.label, EQUAL, p.head.value).vertices.asInstanceOf[java.lang.Iterable[item.Rep]]
+      def apply(rep: tindex.Rep) = {
+        val it = rep.query().has(p.head.property.label, EQUAL, p.head.value).vertices.asInstanceOf[java.lang.Iterable[tindex.item.Rep]]
         it.toList
       }
 
@@ -34,7 +37,7 @@ trait AnyTitanStandardIndex extends AnyIndex { tindex =>
 class TitanStandardIndex[I <: AnyDenotation.Of[T#IndexedType], T <: AnyStandardIndexType](
   val tpe: T,
   val item: I
-  ) extends AnyTitanStandardIndex {
+  ) extends AnyTitanStandardIndex { tindex =>
 
   type Tpe = T
   type Item = I
