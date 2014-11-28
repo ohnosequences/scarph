187307b6268c325362571f42519b9cd46965ec40
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-11-28 21:10:02 +0100

    reorg code, add correct par combinator for paths

--- src/main/scala/ohnosequences/scarph/Combinators.scala
+++ src/main/scala/ohnosequences/scarph/Combinators.scala
@@ -1,33 +1,41 @@
 package ohnosequences.scarph
 
-trait AnyPathCombinator {
+trait AnyPathCombinator {}
 
-
-}
-
-
-trait AnyParPath {
+trait AnyParPath extends AnyPath {
 
   type First <: AnyPath
   val first: First
   type Second <: AnyPath
   val second: Second
 
-  type InT = ParV[first.InT, second.InT]
-  val inT = ParV(first.inT, second.inT)
+  type InT <: ParV[First#In, Second#In]
+  val inT: InT
+  type InC = ExactlyOne.type
+  lazy val inC: InC = ExactlyOne 
+  type In <: ParV[First#In, Second#In]
+  val in: In
 
-  type In = ParV[first.In, second.In]
-  val In = ParV(first.in, second.in)
-
-  type OutT = ParV[first.OutT, second.OutT]
-  val outT = ParV(first.outT, second.outT)
-
-  type Out = ParV[first.Out, second.Out]
-  val Out = ParV(first.out, second.out)
+  type OutT <: ParV[First#Out, Second#Out]
+  val outT: OutT
+  type OutC = ExactlyOne.type
+  lazy val outC: OutC = ExactlyOne
+  type Out <: ParV[First#Out, Second#Out]
+  val out: Out
 }
 
 case class Par[F <: AnyPath, S <: AnyPath](val first: F, val second: S) extends AnyParPath {
 
   type First = F
   type Second = S
+
+  type InT = ParV[First#In, Second#In]
+  lazy val inT: ParV[First#In, Second#In] = ParV(first.in, second.in)
+  type In = ParV[First#In, Second#In]
+  lazy val in: ParV[First#In, Second#In] = ParV(first.in, second.in)
+
+  type OutT = ParV[First#Out, Second#Out]
+  lazy val outT: OutT = ParV(first.out, second.out)
+  type Out = ParV[First#Out, Second#Out]
+  lazy val out: Out = ParV(first.out, second.out)
 }
