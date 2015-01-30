e2e5823033dd3dc0bfbb1dedda63a67e95abec37
Alexey Alekhin <laughedelic@gmail.com>
2015-01-30 19:26:06 +0100

    Syntax refactoring

--- src/main/scala/ohnosequences/scarph/syntax/Paths.scala
+++ src/main/scala/ohnosequences/scarph/syntax/Paths.scala
@@ -8,169 +8,15 @@ object paths {
   import s.graphTypes._, s.steps._, s.predicates._, s.schemas._, s.indexes._
 
 
-  implicit def graphTypeOps[T <: AnyGraphType](t: T):
-        GraphTypeOps[T] =
-    new GraphTypeOps[T](t)
+  implicit def graphTypeSyntax[F <: AnyGraphType](f: F):
+        GraphTypeSyntax[F] =
+    new GraphTypeSyntax[F](f)
 
-  class GraphTypeOps[T <: AnyGraphType](t: T) {
+  class GraphTypeSyntax[F <: AnyGraphType](f: F) {
 
-    def fork: Fork[T] = Fork(t)
+    def fork: Fork[F] = Fork(f)
 
-    // def fork[S <: AnyGraphType { type In = T ⊕ T }](s: S):
-    //   Fork[T] >=> S =
-    //   Fork(t) >=> s
-  }
-
-
-  implicit def biproductOps[TT <: AnyBiproduct](tt: TT):
-        BiproductOps[TT] =
-    new BiproductOps[TT](tt)
-
-  class BiproductOps[TT <: AnyBiproduct](tt: TT) {
-
-    def merge: Merge[TT] = Merge(tt)
-  }
-
-
-  /* Graph/schema ops */
-  implicit def schemaOps[S <: AnyGraphSchema](s: S):
-        SchemaOps[S] =
-    new SchemaOps[S](s)
-
-  class SchemaOps[S <: AnyGraphSchema](s: S) {
-
-    def query[P <: AnyPredicate](p: P): 
-      GraphQuery[S, P] = 
-      GraphQuery(s, p)
-
-    /* This method takes also an index and checks that the predicate satisfies the 
-       index'es restriction, ensuring that it can be utilized for this query */
-    def query[I <: AnyIndex, P <: AnyPredicate](i: I, p: P)
-      (implicit ch: I#PredicateRestriction[P]): 
-        GraphQuery[S, P] =
-        GraphQuery(s, p)
-  }
-
-
-  /* Element types */
-  // implicit def elementOps[E <: AnyGraphElement](e: E):
-  //       ElementOps[E] =
-  //   new ElementOps[E](e)
-
-  // class ElementOps[E <: AnyGraphElement](e: E) {
-
-  //   def get[P <: AnyGraphProperty { type Owner = E }](p: P): Get[P] = Get(p)
-  // }
-
-  implicit def pathElementOps[F <: AnyGraphType { type Out <: AnyGraphElement }](f: F):
-        PathElementOps[F] =
-    new PathElementOps[F](f)
-
-  class PathElementOps[F <: AnyGraphType { type Out <: AnyGraphElement }](f: F) {
-
-    def get[P <: AnyGraphProperty { type Owner = F#Out }](p: P):
-      F >=> Get[P] =
-      f >=> Get(p)
-  }
-
-  /* Edge types */
-  // implicit def edgeOps[E <: AnyEdge](e: E):
-  //       EdgeOps[E] =
-  //   new EdgeOps[E](e)
-
-  // class EdgeOps[E <: AnyEdge](e: E) {
-
-  //   def src: Source[E] = s.steps.Source(e)
-  //   def tgt: Target[E] = s.steps.Target(e)
-  // }
-
-  implicit def pathEdgeOps[F <: AnyGraphType { type Out <: AnyEdge }](f: F):
-        PathEdgeOps[F] =
-    new PathEdgeOps[F](f)
-
-  class PathEdgeOps[F <: AnyGraphType { type Out <: AnyEdge }](f: F) {
-
-    // NOTE: in gremlin this is called .outV
-    def src: F >=> Source[F#Out] = 
-             f >=> Source(f.out)
-
-    // NOTE: in gremlin this is called .inV
-    def tgt: F >=> Target[F#Out] =
-             f >=> Target(f.out)
-  }
-
-  /* Vertex types */
-  // implicit def vertexOps[V <: AnyVertex](v: V):
-  //       VertexOps[V] =
-  //   new VertexOps[V](v)
-
-  // class VertexOps[V <: AnyVertex](v: V) {
-
-  //   // NOTE: this implicit conversion allows us to use edges for predicates on them
-  //   def inE[X, P <: AnyPredicate { type Element <: AnyEdge.To[V] }](x: X)
-  //     (implicit fromX: X => P): InE[P] = InE(fromX(x))
-
-  //   def inV[X, P <: AnyPredicate { type Element <: AnyEdge.To[V] }](x: X)
-  //     (implicit fromX: X => P): InV[P] = InV(fromX(x))
-
-
-  //   def outE[X, P <: AnyPredicate { type Element <: AnyEdge.From[V] }](x: X)
-  //     (implicit fromX: X => P): OutE[P] = OutE(fromX(x))
-
-  //   def outV[X, P <: AnyPredicate { type Element <: AnyEdge.From[V] }](x: X)
-  //     (implicit fromX: X => P): OutV[P] = OutV(fromX(x))
-  // }
-
-  implicit def pathVertexOps[F <: AnyGraphType { type Out <: AnyVertex }](f: F):
-        PathVertexOps[F] =
-    new PathVertexOps[F](f)
-
-  class PathVertexOps[F <: AnyGraphType { type Out <: AnyVertex }](f: F) {
-
-    def inE[X, P <: AnyPredicate { type Element <: AnyEdge.To[F#Out] }](x: X)
-      (implicit fromX: X => P): F >=> InE[P] = f >=> InE(fromX(x))
-
-    def inV[X, P <: AnyPredicate { type Element <: AnyEdge.To[F#Out] }](x: X)
-      (implicit fromX: X => P): F >=> InV[P] = f >=> InV(fromX(x))
-
-
-    def outE[X, P <: AnyPredicate { type Element <: AnyEdge.From[F#Out] }](x: X)
-      (implicit fromX: X => P): F >=> OutE[P] = f >=> OutE(fromX(x))
-
-    def outV[X, P <: AnyPredicate { type Element <: AnyEdge.From[F#Out] }](x: X)
-      (implicit fromX: X => P): F >=> OutV[P] = f >=> OutV(fromX(x))
-  }
-
-  /* This gives user nice warnings and doesn't add unnecessary constructors */
-  // implicit def pathWarnOps[F <: AnyGraphType { type Out <: AnyGraphType { type Container = ExactlyOne }}](f: F): 
-  //       PathWarnOps[F] =
-  //   new PathWarnOps[F](f)
-
-  // class PathWarnOps[F <: AnyGraphType { type Out <: AnyGraphType { type Container = ExactlyOne }}](f: F) {
-  //   @deprecated("You are trying to 'flatten' a non-nested structure, you don't need it", "")
-  //   def flatten: F = f
-
-  //   @deprecated("You are trying to 'map' over one value, you don't need it", "")
-  //   def map[S <: AnyGraphType](s: S)
-  //     (implicit cmp: F#Out ≃ S#In): F >=> S = f >=> s
-
-  //   @deprecated("You are trying to 'flatMap' over one value, you don't need it", "")
-  //   def flatMap[S <: AnyGraphType](s: S)
-  //     (implicit cmp: F#Out ≃ S#In): F >=> S = f >=> s
-
-  //   @deprecated("You are trying to 'forkMap' over one value, you don't need it (use simple 'fork' method instead)", "")
-  //   def forkMap[S <: AnyPar { type In = ParType[F#Out, F#Out] }](s: S):
-  //     Fork[F] >=> S = Composition(Fork(f), s)
-  //     // Fork(f) >=> s
-
-  // }
-
-  /* Any paths */
-  implicit def pathOps[F <: AnyGraphType](f: F): 
-        PathOps[F] =
-    new PathOps[F](f)
-
-  class PathOps[F <: AnyGraphType](f: F) {
+    def merge: Merge[F] = Merge(f)
 
     // F        : A → B
     //        S :     B ⊗ B → C
@@ -196,6 +42,87 @@ object paths {
     def right[S <: AnyBiproduct { type Right <: AnyGraphType { type In = F#Out } }](s: S):
       F >=> S#Right =
       f >=> s.right
+  }
+
+  implicit def biproductSyntax[F <: AnyGraphType { type Out <: AnyBiproduct }](f: F):
+        BiproductSyntax[F] =
+    new BiproductSyntax[F](f)
+
+  class BiproductSyntax[F <: AnyGraphType { type Out <: AnyBiproduct }](f: F) {
+
+    // def merge[S <: AnyGraphType { type In = Merge[F]#Out }](s: S):
+    //   Merge[F] >=> S =
+    //   Merge(f) >=> s
+  }
+
+
+  /* Graph/schema ops */
+  implicit def schemaSyntax[S <: AnyGraphSchema](s: S):
+        SchemaSyntax[S] =
+    new SchemaSyntax[S](s)
+
+  class SchemaSyntax[S <: AnyGraphSchema](s: S) {
+
+    def query[P <: AnyPredicate](p: P): 
+      GraphQuery[S, P] = 
+      GraphQuery(s, p)
+
+    /* This method takes also an index and checks that the predicate satisfies the 
+       index'es restriction, ensuring that it can be utilized for this query */
+    def query[I <: AnyIndex, P <: AnyPredicate](i: I, p: P)
+      (implicit ch: I#PredicateRestriction[P]): 
+        GraphQuery[S, P] =
+        GraphQuery(s, p)
+  }
+
+
+  /* Element types */
+  implicit def elementSyntax[F <: AnyGraphType { type Out <: AnyGraphElement }](f: F):
+        ElementSyntax[F] =
+    new ElementSyntax[F](f)
+
+  class ElementSyntax[F <: AnyGraphType { type Out <: AnyGraphElement }](f: F) {
+
+    def get[P <: AnyGraphProperty { type Owner = F#Out }](p: P):
+      F >=> Get[P] =
+      f >=> Get(p)
+  }
+
+  /* Edge types */
+  implicit def edgeSyntax[F <: AnyGraphType { type Out <: AnyEdge }](f: F):
+        EdgeSyntax[F] =
+    new EdgeSyntax[F](f)
+
+  class EdgeSyntax[F <: AnyGraphType { type Out <: AnyEdge }](f: F) {
+
+    // NOTE: in gremlin this is called .outV
+    def src: F >=> Source[F#Out] = 
+             f >=> Source(f.out)
+
+    // NOTE: in gremlin this is called .inV
+    def tgt: F >=> Target[F#Out] =
+             f >=> Target(f.out)
+  }
+
+  /* Vertex types */
+  implicit def vertexSyntax[F <: AnyGraphType { type Out <: AnyVertex }](f: F):
+        VertexSyntax[F] =
+    new VertexSyntax[F](f)
+
+  class VertexSyntax[F <: AnyGraphType { type Out <: AnyVertex }](f: F) {
+
+    def inE[X, P <: AnyPredicate { type Element <: AnyEdge.To[F#Out] }](x: X)
+      (implicit fromX: X => P): F >=> InE[P] = f >=> InE(fromX(x))
+
+    def inV[X, P <: AnyPredicate { type Element <: AnyEdge.To[F#Out] }](x: X)
+      (implicit fromX: X => P): F >=> InV[P] = f >=> InV(fromX(x))
+
+
+    def outE[X, P <: AnyPredicate { type Element <: AnyEdge.From[F#Out] }](x: X)
+      (implicit fromX: X => P): F >=> OutE[P] = f >=> OutE(fromX(x))
+
+    def outV[X, P <: AnyPredicate { type Element <: AnyEdge.From[F#Out] }](x: X)
+      (implicit fromX: X => P): F >=> OutV[P] = f >=> OutV(fromX(x))
   }
 
 }
