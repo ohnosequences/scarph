963393617fc395df9cf32de8398e88b218f28af4
Alexey Alekhin <laughedelic@gmail.com>
2015-04-01 17:57:42 +0200

    Made GraphSchema a class (not case)

--- src/main/scala/ohnosequences/scarph/Schemas.scala
+++ src/main/scala/ohnosequences/scarph/Schemas.scala
@@ -4,15 +4,16 @@ object schemas {
 
   import graphTypes._
 
+
   trait AnyGraphSchema extends AnyGraphObject {
 
-    val  vertices: Set[AnyVertex]
-    val  edges: Set[AnyEdge]
-    val  valueTypes: Set[AnyValueType]
-    val  properties: Set[AnyGraphProperty]
+    val vertices: Set[AnyVertex]
+    val edges: Set[AnyEdge]
+    val valueTypes: Set[AnyValueType]
+    val properties: Set[AnyGraphProperty]
   }
 
-  case class GraphSchema(
+  class GraphSchema(
     val label: String,
     val vertices: Set[AnyVertex],
     val edges: Set[AnyEdge],
