c88f276e638d23c727fd16c1aaf334e454f47263
Alexey Alekhin <laughedelic@gmail.com>
2017-03-06 21:12:14 +0100

    Draft code for writing interface

--- src/main/scala/ohnosequences/scarph/objects.scala
+++ src/main/scala/ohnosequences/scarph/objects.scala
@@ -38,8 +38,15 @@ sealed trait AnyGraphElement extends AnyGraphObject
 
 /* A vertex is a simple graph object representing some type of entities */
 trait AnyVertex extends AnyGraphElement
+
 abstract class Vertex(val label: String) extends AnyVertex
 
+// /* You can use this class to automize vertices labeling. It has to be instantiated as a `case object` and its name will be used as a base label. If you want to transform it (e.g. add a prefix) provide an implicit function for relabeling. */
+// abstract class vertex(implicit relabel: String => String = identity)
+//   extends AnyVertex { _ : Singleton with Product =>
+//
+//     final val label: String = relabel(this.productPrefix)
+//   }
 
 
 /* An edge is an object representing relation between vertex-objects */
